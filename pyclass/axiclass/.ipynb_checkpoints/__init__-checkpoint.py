74f7ef1b4f7234914c4a10b3d69722f93b948e28
arnaud de mattia <adematti@dphppcz231.extra.cea.fr>
2024-04-12 10:34:57 +0200

    clean up

--- pyclass/axiclass/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-from .utils import get_external_files, load_precision, load_ini
-from .binding import (ClassEngine, Background, Thermodynamics, Primordial, Perturbations, Transfer, Harmonic, Fourier,
-                      ClassInputError, ClassComputationError, ClassRuntimeError)
